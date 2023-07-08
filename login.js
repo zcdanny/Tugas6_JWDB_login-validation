62e3736ef6aa44ee4d27db0b032ef4d4bf72d8a0
zcdanny <100658079+zcdanny@users.noreply.github.com>
2023-07-08 13:50:24 +0700

    Add files via upload

---
+++ login.js
@@ -0,0 +1,19 @@
+const correctEmail = "RamandaDanny@gmail.com";
+const correctPassword = "1234";
+
+const messageContainer = document.getElementById('message-container');
+
+function cekLogin(){
+    const userEmail = document.getElementById('fEmail').
+    value;
+
+    const userPassword = document.getElementById('fPassword').value;
+
+    if(userEmail != correctEmail || (userPassword != correctPassword)) {
+        messageContainer.innerHTML = "<p class='error-message'>Login Gagal, Email atau Password salah ! </p>";
+    } else {
+
+        messageContainer.innerHTML = "<p class='success-message'>Login Berhasil...</p>";
+        //window.location.href = "order/order.html";
+    }
+}
